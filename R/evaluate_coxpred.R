70b7810b3349b9ac5c718692831999556369a5bc
Imad EL BADSIY <elbadisyimad@gmail.com>
2022-06-08 13:46:27 +0100

    typo

--- R/evaluate_coxpred.R
+++ R/evaluate_coxpred.R
@@ -25,13 +25,13 @@ evaluate_coxpred <- function(data, R = 5, seed = 1234, method = "fullcox", scena
     index.train = sample(1:N,2/3*N)
     data.train = dat[index.train,]
     data.test = dat[-index.train,]
-    dis_time = sort(data.train$time[data.train$status == 1])  #the default time points
+    dis_time = sort(data.train$time[data.train$event == 1])  #the default time points
     
     
     ## **** IBS 
-    fitcox = survival::coxph(Surv(time, status)~., data = data.train, x = TRUE)
+    fitcox = survival::coxph(Surv(time, event)~., data = data.train, x = TRUE)
     mat_cox = pec::predictSurvProb(fitcox, data.test, dis_time)
-    surv_obj = survival::Surv(data.test$time, data.test$status)
+    surv_obj = survival::Surv(data.test$time, data.test$event)
     
     IBS[i] = SurvMetrics::IBS(surv_obj, mat_cox, dis_time)
     IBS = as.numeric(unlist(IBS))
@@ -39,7 +39,7 @@ evaluate_coxpred <- function(data, R = 5, seed = 1234, method = "fullcox", scena
     
     #**** Cindex
     med_index = stats::median(1:length(dis_time))
-    surv_obj = survival::Surv(data.test$time, data.test$status)
+    surv_obj = survival::Surv(data.test$time, data.test$event)
     
     Cindex[i] = SurvMetrics::Cindex(surv_obj, predicted = mat_cox[, med_index])
     Cindex = as.numeric(unlist(Cindex))
@@ -48,21 +48,21 @@ evaluate_coxpred <- function(data, R = 5, seed = 1234, method = "fullcox", scena
     ## survAUC::AUC.cd()
     ### Chambless, L. E. and G. Diao (2006). Estimation of time-dependent area under the ROC curve for long-term risk prediction. Statistics in Medicine 25, 3474–3486
     
-    fitcox = survival::coxph(Surv(time, status)~., data = data.train, x = TRUE)
+    fitcox = survival::coxph(Surv(time, event)~., data = data.train, x = TRUE)
     predcox <- predict(fitcox)
     predcoxnew <- predict(fitcox, newdata=data.test)
-    surv_obj <- survival::Surv(data.train$time, data.train$status)
-    surv_obj_new <- survival::Surv(data.test$time, data.test$status)
+    surv_obj <- survival::Surv(data.train$time, data.train$event)
+    surv_obj_new <- survival::Surv(data.test$time, data.test$event)
     
     AUC[i] = survAUC::AUC.cd(surv_obj, surv_obj_new, predcox, predcoxnew, dis_time)$iauc
     AUC = as.numeric(unlist(AUC))
     
     #****
-    fitcox <- survival::coxph(Surv(time, status)~., data = data.train, x = TRUE)
+    fitcox <- survival::coxph(Surv(time, event)~., data = data.train, x = TRUE)
     predcox <- predict(fitcox)
     predcoxnew <- predict(fitcox, newdata=data.test)
-    surv_obj <- survival::Surv(data.train$time, data.train$status)
-    surv_obj_new <- survival::Surv(data.test$time, data.test$status)
+    surv_obj <- survival::Surv(data.train$time, data.train$event)
+    surv_obj_new <- survival::Surv(data.test$time, data.test$event)
     
     predError[i] = predErr(surv_obj, surv_obj_new, predcox, predcoxnew, dis_time, type = "robust", int.type = "weighted")$ierror
     
